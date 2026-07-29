bcda775192a1b870cdb369476c29ba243f6b2869
Emerson Oliveira <emerson.oliverbr@gmail.com>
2026-07-28 21:32:48 -0300

    Entrada de dados

---
+++ Modulo-1-Estrutura-Sequencial/EntradaDeDados/EntradaDeDados/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+
+namespace EntradaDeDados;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double x = 1.35784;
+        int idade = 40;
+        string nomeCompleto = "Emerson Matos Oliveira";
+        char sexo = 'M';
+
+        Console.WriteLine(x);
+        Console.WriteLine(x.ToString("F2"));
+        Console.WriteLine(x.ToString("F4"));
+        Console.WriteLine(x.ToString("F2", CultureInfo.InvariantCulture));
+        Console.WriteLine("+-------------------------------+");
+        Console.WriteLine($"O valor do troco é R$ {x.ToString("F2", CultureInfo.InvariantCulture)} reais");
+        Console.WriteLine($"O funcionario {nomeCompleto} tem {idade} anos e é do sexo, {sexo}");
+    }
+}
