a7f7d7770854f638765c385ba391ef847597df86
Emerson Oliveira <emerson.oliverbr@gmail.com>
2026-07-30 22:33:36 -0300

    Add: outros exemplos

---
+++ Modulo-1-Estrutura-Sequencial/EntradaDeDadosParte2/EntradaDeDadosParte2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Globalization;
+//Entrada de dados parte 2 explicação teoria
+namespace EntradaDeDadosParte2;
+class Program
+{
+    static void Main(String[] args)
+    {
+        int n1 = int.Parse(Console.ReadLine() ?? "0");
+        char inicialNome = char.Parse(Console.ReadLine() ?? "0");
+        double n2 = double.Parse(Console.ReadLine() ?? "0");
+
+        Console.WriteLine("Voce digitou:");
+        Console.WriteLine($"{n1}");
+        Console.WriteLine($"{inicialNome}");
+        Console.WriteLine($"{n2}");
+        Console.WriteLine($"{n2.ToString("F2", CultureInfo.InvariantCulture)}");
+
+        Console.WriteLine("---------------");
+
+        string[] dadosPessoais = Console.ReadLine().Split(' ');
+        string? nome = dadosPessoais[0];
+        char sexo = char.Parse(dadosPessoais[1]);
+        int idade = int.Parse(dadosPessoais[2]);
+        double altura = double.Parse(dadosPessoais[3], CultureInfo.InvariantCulture);
+        Console.WriteLine("---------------");
+
+        Console.WriteLine("Dados do cliente: ");
+        Console.WriteLine($"{nome}");
+        Console.WriteLine($"{sexo}");
+        Console.WriteLine($"{idade}");
+        Console.WriteLine($"{altura.ToString("F2",CultureInfo.InvariantCulture)}");
+    }
+}
---
+++ Modulo-1-Estrutura-Sequencial/ExercicioTerreno/ExercicioTerreno/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+
+//Exercicio fixação explica,ão teoria 
+
+namespace ExercicioTerreno;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        double comprimento = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        double valorMetroQuadrado = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        Console.WriteLine("--------------");
+        double area = largura * comprimento;
+        double preco = area * valorMetroQuadrado;
+
+        Console.WriteLine($"AREA: {area}");
+        Console.WriteLine($"PRECO: {preco.ToString("F2",CultureInfo.InvariantCulture)}");
+
+        Console.ReadKey();
+    }
+}
